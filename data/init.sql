2583be164ee6cf8bc513ff24aee7d1a33c7bf56c
KituNaranjo <129440787+KituNaranjo@users.noreply.github.com>
2024-01-05 18:47:17 -0500

    Create init.sql

--- data/init.sql
+++ data/init.sql
@@ -1,1 +1,6 @@
+CREATE TABLE tareas (id SERIAL PRIMARY KEY, nombre VARCHAR(255), tarea VARCHAR(255) NOT NULL);
 
+INSERT INTO tareas (nombre, tarea) VALUES ('Christian', 'Conexion Backend con Frontend');
+INSERT INTO tareas (nombre, tarea) VALUES ('Leandro', 'Revisar el Frontend');
+INSERT INTO tareas (nombre, tarea) VALUES ('Sara', 'Diseño del Frontend');
+INSERT INTO tareas (nombre, tarea) VALUES ('Bryan', 'Database');
